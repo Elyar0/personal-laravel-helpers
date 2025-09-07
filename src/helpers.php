add774b990e4c6d94bdbfa915890bac0454afcc4
Elyar <elyareranjbar@gmail.com>
2025-09-07 15:25:53 +0330

    prevInArray function added

--- src/helpers.php
+++ src/helpers.php
@@ -13,11 +13,28 @@ if (!function_exists('nextInArray')) {
     function nextInArray(array $array, mixed $current)
     {
         if (is_null($current)) return $array[0] ?? null;
+
         $index = array_search($current, $array);
         if ($index === false || empty($array)) {
             return null;
         }
+
         $nextIndex = ($index + 1) % count($array);
         return $array[$nextIndex];
+    }
+}
+
+if (!function_exists('prevInArray')) {
+    function prevInArray(array $array, mixed $current)
+    {
+        if (is_null($current)) return $array[count($array) - 1] ?? null;
+
+        $index = array_search($current, $array);
+        if ($index === false || empty($array)) {
+            return null;
+        }
+
+        $prevIndex = ($index - 1 + count($array)) % count($array);
+        return $array[$prevIndex];
     }
 }
